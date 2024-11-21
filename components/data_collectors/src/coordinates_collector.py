4de1fd8a36a160fba91e4a87d24024a700a95487
Christian <christianresch@mailbox.org>
2024-11-21 18:51:48 -0500

    Wrote first tests for Coordinates Collector and Outline of Coordinates Collector

---
+++ components/data_collectors/src/coordinates_collector.py
@@ -0,0 +1,10 @@
+from typing import Tuple
+import requests
+
+class CoordinatesCollector():
+
+    def __init__(self):
+        pass
+
+    def get_coordinates(self, city_name: str) -> [Tuple[float, float]]:
+        return None
---
+++ components/data_collectors/tests/coordinates_collector_test.py
@@ -0,0 +1,43 @@
+from components.data_collectors.src.coordinates_collector import CoordinatesCollector
+import unittest
+from unittest.mock import MagicMock
+
+class TestCoordinatesCollector(unittest.TestCase):
+
+    def setUp(self):
+        self.coordinates_collector = CoordinatesCollector()
+        self.coordinates_collector.get_coordinates = MagicMock(return_value=(4.60971, -74.08175))
+
+        self.real_coordinates_collector = CoordinatesCollector()
+
+    def test_success(self):
+        latitude, longitude = self.coordinates_collector.get_coordinates('Bogota')
+
+        assert latitude == 4.60971
+        assert longitude == -74.08175
+
+    def test_real_api_call(self):
+        latitude, longitude = self.real_coordinates_collector.get_coordinates('Bogota')
+
+        assert isinstance(latitude, (float)), "Value is not a number"
+        assert isinstance(longitude, (float)), "Value is not a number"
+
+    def test_wrong_user_input(self):
+        self.coordinates_collector.get_coordinates.side_effect = ValueError("Invalid input: Check your parameters.")
+
+        #Check if ValueError is raised when providing wrong input
+        with self.assertRaises(ValueError) as context:
+            self.coordinates_collector.get_coordinates(2024)
+
+    def test_wrong_user_input_with_real_api(self):
+        with self.assertRaises(ValueError) as context:
+            self.real_coordinates_collector.get_coordinates(2024)
+
+        with self.assertRaisesRegex(ValueError, "400"):
+            self.real_coordinates_collector.get_coordinates(2024)
+
+
+
+
+
+#build this based on https://geocoding-api.open-meteo.com/v1/search?name=Paris, see https://open-meteo.com/en/docs/geocoding-api
