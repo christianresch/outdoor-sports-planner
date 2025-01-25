71b1c1720ada8509936622b4ebe69ac517989e0f
Christian <christianresch@mailbox.org>
2025-01-25 17:05:50 -0500

    First version of final web server added

--- applications/web_server/src/app.py
+++ applications/web_server/src/app.py
@@ -1,32 +1,48 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import HTMLResponse
 from starlette.responses import Response
-from components.data_collectors.src.weather_data_collector import WeatherDataCollector
-from components.data_gateways.src.weather_data_gateway import WeatherDataGateway
-from components.data_collectors.src.coordinates_collector import CoordinatesCollector
+from loguru import logger
+import httpx
 import os
 
 app = FastAPI()
+
+# TODO Adapt this!
+DATA_ANALYZER_API_URL = "http://localhost:8003/analyze"
 
 @app.get("/", response_class=HTMLResponse)
 async def main():
     return '''
     
     <h1> Outdoor sports planner</h1>
-    <h2> Get the best outdoor sport days based on air quality</h2>
+    <h2> Get the best outdoor sport days based on air quality and weather forecast</h2>
     
     Enter your location here: <br>
     <br>
-     <form action="/dummy_air_quality_data" method="POST">
+     <form action="/best_outdoor_sports_day" method="POST">
          <input name="user_input">
          <input type="submit" value="Submit!">
      </form>
     (Note: This is just a dummy website so far and won't actually give you real information)<br>
     
      '''
-@app.post("/dummy_air_quality_data")
-async def dummy_air_quality_data(user_input: str = Form(...)):
+@app.post("/best_outdoor_sports_day")
+async def best_outdoor_sports_day(user_input: str = Form(...)):
     user_input = user_input.capitalize()
+
+    logger.info(f"Request received with city: {user_input}, latitude: to be added and longitude: to be added")
+
+    params = {}
+    params["city"] = user_input
+
+    async with httpx.AsyncClient() as client:
+        logger.info(f"Fetching best sports day with params: {params}")
+        data_analyzer_response = await client.post(DATA_ANALYZER_API_URL, json=params)
+        if data_analyzer_response.status_code != 200:
+            raise HTTPException(status_code=data_analyzer_response.status_code, detail="Error fetching best sports day.")
+        logger.info(f"Best sports day received with {data_analyzer_response.status_code} status code")
+        data_analyzer_response = data_analyzer_response.json()
+
     html_content = f"""
     <!DOCTYPE html>
     <html lang="en">
@@ -34,36 +50,8 @@ async def dummy_air_quality_data(user_input: str = Form(...)):
     </head>
     <body>
         <h1> Your outdoor sports prediction for {user_input}</h1>
-        You are in <b>{user_input}</b>. [PLACEHOLDER FOR CONTENT]</h1>
+        You are in <b>{user_input}</b>. Your best sports day is {str(data_analyzer_response[0]['date'])}</h1>
     """
     return Response(content=html_content, media_type='text/html')
 
 #TODO Add /health-check endpoint, see Production Monitoring Video and a metrics endpoint (Prometheus in the example)
-
-if __name__ == "__main__":
-    #TODO Aggregate this into workers and tasks (see REST API video in the course)
-    weather_collector = WeatherDataCollector()
-    coordinates_collector = CoordinatesCollector()
-
-    gateway = WeatherDataGateway(db_path="sqlite:///test_weather.db")
-
-    if not os.path.exists("weather.db"):
-        gateway.create()
-
-    city = "Bogota"
-
-    latitude, longitude = coordinates_collector.get_coordinates(city)
-
-    temperature = weather_collector.get_weather_data(latitude, longitude)
-
-    gateway.insert_weather_data(city, latitude, longitude, temperature)
-
-    inserted_record = gateway.get_weather_data_by_city(city)[0]
-
-    print(inserted_record.city)
-    print(inserted_record.latitude)
-    print(inserted_record.longitude)
-    print(inserted_record.temperature)
-    print(inserted_record.recorded_at)
-
-
